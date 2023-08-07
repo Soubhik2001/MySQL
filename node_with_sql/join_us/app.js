ae76e50ea8e4b9f2407b131cb44e6a0dab82a70b
Soubhik <gsoubhik01@gmail.com>
2023-08-07 10:38:52 +0530

    07.08.23

--- node_with_sql/join_us/app.js
+++ node_with_sql/join_us/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const ejs = require("ejs");
 const mysql=require ("mysql");
-
+const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extendesd:true}));
+app.use(express.static("public"));
 
 const connection=mysql.createConnection({
     host:"localhost",
@@ -23,6 +25,17 @@ app.get("/", function (req, res) {
     // res.send(msg);
   });
 });
+
+
+app.post('/register', function(req,res){
+  var person = {email: req.body.email};
+  connection.query('INSERT INTO users SET ?', person, function(err, result) {
+  console.log(err);
+  console.log(result);
+  res.redirect("/");
+  });
+ });
+
 
 app.listen(3000, function () {
   console.log("Server running on 3000!");
